03d608b4d17645068c3167fbf1eeb22401d79c77
Ernst Stolz <eric@spiritsdontfly.com>
2024-02-23 08:42:52 +0100

    refactor for split routes

--- app.js
+++ app.js
@@ -1,137 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const postgres = require("@vercel/postgres");
+const routes = require("./routes");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/", async (request, response) => {
-  createTables();
-  //table was created => load data
-  const { rows } = await postgres.sql`SELECT * FROM notes`;
-  return response.json(rows);
-});
-
-app.get("/:id", async (request, response) => {
-  createTables();
-  //table was created => load data
-  const { id } = request.params;
-  const { rows } = await postgres.sql`SELECT * FROM notes WHERE id = ${id}`;
-
-  if (!rows.length) {
-    return response.json({ error: "Note not found." });
-  }
-
-  return response.json(rows[0]);
-});
-
-app.post("/", async (request, response) => {
-  createTables();
-  const { content } = request.body;
-  if (content) {
-    await postgres.sql`INSERT INTO notes (content) VALUES (${content})`;
-    response.json({ message: "Successfully created note." });
-  } else {
-    response.json({ error: "Note NOT created. Content is missing." });
-  }
-});
-
-/* vegan delete route */
-app.delete("/:tofu", async (request, response) => {
-  createTables();
-  /* const  tofu  = request.params.tofu; */
-  const { tofu } = request.params;
-  const { rowCount } = await postgres.sql`DELETE FROM notes WHERE id = ${tofu}`;
-
-  if (!rowCount) {
-    return response.json({ error: "Note not found." });
-  }
-
-  response.json({ message: "Successfully deleted note." });
-});
-
-app.put("/:id", async (req, res) => {
-  createTables();
-  const id = req.params.id;
-  const { content } = req.body;
-
-  const { rowCount } =
-    await postgres.sql`UPDATE notes SET content = ${content} WHERE id=${id}`;
-
-  if (!rowCount) {
-    return res.json({ error: "note not found" });
-  }
-
-  return res.json("Successfully edited the note.");
-});
-
-app.get("/users/:user", async (req, res) => {
-  createTables();
-  /* const  user  = req.params.user; */
-  const { user } = req.params;
-
-  /* select all notes from a specific user */
-  const { rows } =
-    await postgres.sql`SELECT * FROM users LEFT JOIN notes ON notes."userId" = users.id WHERE users.name = ${user}`;
-
-  return res.json(rows);
-});
-
-app.get("/users/:user/:id", async (req, res) => {
-  createTables();
-  const { user, id } = req.params;
-
-  /* select a single note from a specific user */
-  const { rows } =
-    await postgres.sql`SELECT * FROM users LEFT JOIN notes ON notes."userId" = users.id WHERE users.name = ${user} AND notes.id = ${id}`;
-
-  if (!rows.length) {
-    return res.json({ message: "note not found" });
-  }
-
-  return res.json(rows[0]);
-});
-
-/*
- * - create another route with the method PUT
- * - specific route => "/users/:user/:id"
- * - update note of the incoming id and user
- */
-app.put("/users/:user/:id", async (req, res) => {
-  await createTables();
-  const user = req.params.user; //eric
-  const notesId = req.params.id; //5
-  const { content } = req.body;
-
-  if (content) {
-    /* first check to see if we can find the user */
-    const {
-      rows: [{ id }],
-    } = await postgres.sql`SELECT id FROM users WHERE users.name = ${user}`;
-
-    /* 
-    { rowCount: 1, 
-        rows: [ { id:1 } ] 
-    }
-    */
-
-    /* then use that user's id to update the requested note */
-    const { rowCount } =
-      await postgres.sql`UPDATE notes SET content = ${content} WHERE notes."userId" = ${id} AND notes.id = ${notesId}`;
-
-    if (!rowCount) {
-      return res.json({ error: "note not found" });
-    }
-
-    return res.json("Successfully edited note");
-  } else {
-    return res.json("Note NOT created since content is missing.");
-  }
-});
+app.use("/", routes);
 
 // default catch-all handler
 app.get("*", (request, response) => {
@@ -144,21 +22,3 @@ const server = app.listen(port, () =>
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
-
-async function createTables() {
-  await postgres.sql`
-      CREATE TABLE IF NOT EXISTS users (
-          id SERIAL PRIMARY KEY,
-          name VARCHAR(255) UNIQUE NOT NULL,
-          "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-          );
-      `;
-  await postgres.sql`
-        CREATE TABLE IF NOT EXISTS notes (
-            id SERIAL PRIMARY KEY,
-            content VARCHAR(255) NOT NULL,
-            "userId" integer REFERENCES users (id),
-            "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-            );
-        `;
-}
